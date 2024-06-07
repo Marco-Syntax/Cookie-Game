7cb98bd95fde9096e9ec430a0a965fa3c7e5992a
Marco-Syntax <mgrimme954@gmail.com>
2024-06-07 21:58:26 +0200

    CustomButton erstellt

--- CookieGame/CookieGame/Models/CustomButton.swift
+++ CookieGame/CookieGame/Models/CustomButton.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+import SwiftUI
+struct CustomButton: View {
+    var title: String
+    var backgroundColor: Color
+    var action: () -> Void  // Ein Closure, das eine Funktion für die Aktion beim Drücken eines Buttons wie StartTimer() oder StopTimer() erwartet
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .padding(.horizontal, 30)
+                .padding(.vertical, 10)
+                .background(backgroundColor)
+                .foregroundStyle(.white)
+                .cornerRadius(6)
+                .shadow(color: Color.black.opacity(0.6), radius: 4, x: 0, y: 2)
+        }
+    }
+}
